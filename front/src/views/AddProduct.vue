44407f50b7bd98f67bd37675a5f9d326110ab053
samarbahloul <88493580+samarbahloul@users.noreply.github.com>
2022-04-20 01:27:25 +0100

    Create AddProduct.vue

---
+++ front/src/views/AddProduct.vue
@@ -0,0 +1,16 @@
+<template>
+  <AddForme />
+</template>
+
+<script>
+import AddForme from "@/components/AddForme.vue";
+export default {
+  name: "AddProduct",
+  components: {
+    AddForme,
+  },
+};
+</script>
+
+<style>
+</style>
